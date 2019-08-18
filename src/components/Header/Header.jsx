77812c0cb8751189ac4354bab8bf6b17a1d6c3de
Romain <romain@romains-macbook-pro.home>
2019-08-18 21:36:05 +0200

    Updated Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 
-import { Container, Row, Col } from "reactstrap";
-
 import "../../assets/styles/Header.css";
 
 export default class Header extends Component {
