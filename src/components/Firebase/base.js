fcd4bf7fbc35a80e1b265b9db2ab15a875c179a1
Romain <romain@romains-macbook-pro.home>
2019-08-18 21:55:57 +0200

    Set up Firebase

---
+++ src/components/Firebase/base.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+
+const app = firebase.initializeApp({
+	apiKey: "AIzaSyB0UDkY-CCyMz7d79kjpbeqD5Ka-Bgu_j0",
+	authDomain: "react-rapport-stage.firebaseapp.com",
+	databaseURL: "https://react-rapport-stage.firebaseio.com",
+	projectId: "react-rapport-stage",
+	storageBucket: "react-rapport-stage.appspot.com",
+	messagingSenderId: 937437986699
+});
+
+export default app;
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,76 +2,41 @@ import React, { Component } from "react";
 
 import { Container } from "reactstrap";
 
+import app from "../components/Firebase/base";
+
 import Header from "../components/Header/Header";
+import TimelineItem from "../components/Timeline/TimelineItem";
 
 import "../assets/styles/Timeline.css";
 export default class Project extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			projects: []
+		};
+	}
+
+	componentDidMount() {
+		let data = app.database().ref("projects");
+		data.on("value", snapshot => {
+			let projects = snapshot.val();
+
+			console.log("projects: ", projects);
+			this.setState({ projects });
+		});
+	}
+
 	render() {
 		return (
 			<main>
-				<Header title={"Projects"} />
+				<Header title='Projects' />
 
 				<Container>
 					<ul className='timeline'>
-						<li className='timeline-item bg-white rounded ml-3 p-4 shadow'>
-							<div className='timeline-arrow' />
-							<h2 className='h5 mb-0'>Title of section 1</h2>
-							<span className='small text-gray'>
-								<i className='fa fa-clock-o mr-1' />
-								21 March, 2019
-							</span>
-							<p className='text-small mt-2 font-weight-light'>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non
-								nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis
-								sagittis ligula in sodales vehicula....
-							</p>
-						</li>
-						<li className='timeline-item bg-white rounded ml-3 p-4 shadow'>
-							<div className='timeline-arrow' />
-							<h2 className='h5 mb-0'>Title of section 2</h2>
-							<span className='small text-gray'>
-								<i className='fa fa-clock-o mr-1' />5 April, 2019
-							</span>
-							<p className='text-small mt-2 font-weight-light'>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non
-								nisi semper.
-							</p>
-							<p className='text-small mt-2 font-weight-light'>
-								Libero expedita explicabo eius fugiat quia aspernatur autem laudantium error architecto
-								recusandae natus sapiente sit nam eaque, consectetur porro molestiae ipsam! Deleniti.
-							</p>
-						</li>
-						<li className='timeline-item bg-white rounded ml-3 p-4 shadow'>
-							<div className='timeline-arrow' />
-							<h2 className='h5 mb-0'>Title of section 3</h2>
-							<span className='small text-gray'>
-								<i className='fa fa-clock-o mr-1' />
-								18 August, 2019
-							</span>
-							<p className='text-small mt-2 font-weight-light'>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non
-								nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis
-								sagittis ligula in sodales vehicula....
-							</p>
-						</li>
-						<li className='timeline-item bg-white rounded ml-3 p-4 shadow'>
-							<div className='timeline-arrow' />
-							<h2 className='h5 mb-0'>Title of section 4</h2>
-							<span className='small text-gray'>
-								<i className='fa fa-clock-o mr-1' />
-								10 October, 2019
-							</span>
-							<p className='text-small mt-2 font-weight-light'>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non
-								nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis
-								sagittis ligula in sodales vehicula....
-							</p>
-							<p className='text-small mt-2 font-weight-light'>
-								Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis
-								illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum
-								molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.
-							</p>
-						</li>
+						<TimelineItem />
+						<TimelineItem />
+						<TimelineItem />
+						<TimelineItem />
 					</ul>
 				</Container>
 			</main>
