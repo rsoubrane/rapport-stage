6bef8535c524d6fe13c9b9a063d107a6ab93ac26
Romain <romain@romains-macbook-pro.home>
2019-08-18 17:59:25 +0200

    Init Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Enterprise from "./pages/Enterprise";
+import Project from "./pages/Project";
+import ProjectDetails from "./pages/ProjectDetails";
+import Review from "./pages/Review";
+
 import "./App.css";
 
-function App() {
-	return <div className='App' />;
-}
+export default class App extends Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<Switch>
+					<Route exact path='/' component={Home} />
 
-export default App;
+					<Route exact path='/profile' component={Profile} />
+					<Route exact path='/enterprise' component={Enterprise} />
+					<Route exact path='/projects' component={Project} />
+					<Route exact path='/projects/:id' component={ProjectDetails} />
+					<Route exact path='/review' component={Review} />
+				</Switch>
+			</BrowserRouter>
+		);
+	}
+}
