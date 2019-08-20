76247d43af3988cd51bcf2835f78f2b9135a5225
Romain <romain@Romains-MacBook-Pro.local>
2019-08-20 20:38:34 +0200

    Added Posts to the Home

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+import { Row, Col, Card, CardBody } from "reactstrap";
+import { Link } from "react-router-dom";
+
+export default class Post extends Component {
+	render() {
+		return (
+			<Col md='6'>
+				<Card className='mb-4 box-shadow'>
+					<img
+						className='card-img-right flex-auto d-none d-lg-block'
+						alt='Thumbnail'
+						style={{ width: "100%", height: "225px", display: "block" }}
+						src={this.props.picture}
+					/>
+					<div className='card-body'>
+						<h4 className='mb-2'>
+							<Link className='text-primary' to={this.props.redirection}>
+								{this.props.title}
+							</Link>
+						</h4>
+						<p className='card-text mb-auto'>{this.props.description}</p>
+						<div className='d-flex justify-content-between align-items-center'>
+							<small className='text-muted'>
+								August 20, by <Link to='/profile'>Romain</Link>
+							</small>
+						</div>
+					</div>
+				</Card>
+			</Col>
+		);
+	}
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,12 @@ import React, { Component } from "react";
 import { Container, Row } from "reactstrap";
 
 import Header from "../components/Header/Header";
+import Post from "../components/Post/Post";
 import Article from "../components/Blog/Article";
 import BlogSidebar from "../components/Blog/BlogSidebar";
 
+import enterprise from "../assets/medias/enterprise.jpg";
+import project from "../assets/medias/project.jpg";
 export default class Home extends Component {
 	render() {
 		return (
@@ -14,6 +17,20 @@ export default class Home extends Component {
 
 				<Container>
 					<Row>
+						<Row className='m-3 w-100'>
+							<Post
+								title='Une entreprise en or ?'
+								description="Découvrez-en-plus sur onepoint, l'un des leaders de la transformation digitale"
+								picture={enterprise}
+								redirection='/enterprise'
+							/>
+							<Post
+								title='Des projets intéressants ?'
+								description='Venez décvouvrir à quelle sauce le stagiaire a été mangé'
+								picture={project}
+								redirection='/projects'
+							/>
+						</Row>
 						<Article />
 						<BlogSidebar />
 					</Row>
