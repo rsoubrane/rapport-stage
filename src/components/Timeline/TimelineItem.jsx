5ca9b5a75708a6df5f6ffbc3339b5f43ddaec23b
Romain <romain@Romains-MacBook-Pro.local>
2019-08-20 22:14:05 +0200

    Updated Timeline

--- src/components/Timeline/TimelineItem.jsx
+++ src/components/Timeline/TimelineItem.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 
-import { Link } from "react-router-dom";
-
 import Carrousel from "../Carrousel/Carrousel";
 
 export default class TimelineItem extends Component {
